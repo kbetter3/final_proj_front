bdc20ff4374bb051db562ab8f1117ec6406d377b
kbetter3 <kbetter3@naver.com>
2018-06-08 21:24:09 +0900

    register ajax

--- res/js/my.register.js
+++ res/js/my.register.js
@@ -4,13 +4,28 @@ var register_emailValidCheck = false;
 
 var register_idDupCheck = false;
 
+function register_idCheck() {
+    register_idValidationCheck();
+
+    if (register_idValidCheck) {
+        register_idDuplicationCheck();
+        console.log("iddupchck");
+    } else {
+        return;
+    }
+
+    if (register_idValidCheck && register_idDupCheck) {
+
+    }
+}
+
 function register_idValidationCheck() {
     var regex = /^[a-zA-Z0-9]{4,20}$/;
     var id_input = $("#my-register-id");
     var id = id_input.val();
 
     if (regex.test(id) && id.length >= 4 && id.length <= 20) {
-        id_input.removeClass("my-member-red-border").addClass("my-member-green-border");
+//        id_input.removeClass("my-member-red-border").addClass("my-member-green-border");
 
         $("#my-register-id-info").hide();
 
@@ -20,7 +35,7 @@ function register_idValidationCheck() {
         if (id.length === 0) {
             $("#my-register-id-info").text("").append("아이디를 입력하세요.").show();
         } else {
-            $("#my-register-id-info").hide();
+            $("#my-register-id-info").text("").append("아이디는 영문+숫자조합 4~20자만 가능합니다.").show();
         }
 
         id_input.removeClass("my-member-green-border").addClass("my-member-red-border");
@@ -31,12 +46,10 @@ function register_idValidationCheck() {
 }
 
 function register_idDuplicationCheck() {
-    register_idValidationCheck();
-
     if (register_idValidCheck) {
         var cc = $.ajax({
             url: 'iddupcheck',
-            data: {id : $("#my-register-id")},
+            data: {id : $("#my-register-id").val()},
             success: register_idDuplicationCheck
         });
 
@@ -50,4 +63,9 @@ function register_idDuplicationCheckSuccess(data) {
     } else {
         console.log("data is null");
     }
+}
+
+function register_resetIdValidation() {
+    register_idValidCheck = false;
+    register_idDupCheck = false;
 }
