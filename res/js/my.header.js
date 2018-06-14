42cabf2e0b4f1c064c5d2382012683047ebcf7e8
kbetter3 <kbetter3@naver.com>
2018-06-14 21:51:09 +0900

    동적 태그 생성

--- res/js/my.header.js
+++ res/js/my.header.js
@@ -6,6 +6,22 @@ function my_header_home() {
 }
 
 function my_header_success_home(json) {
-    var jobj = $.parseJSON(json);
-    console.log(jobj.data);
+//    var jobj = $.parseJSON(json);
+    console.log(json.data);
+}
+
+function my_header_voucher() {
+
+}
+
+function my_header_login() {
+    $("#my-header-right-container").html($("<div class='my-header-logout'>logout</div>").on("click", my_header_logout));
+}
+
+function my_header_register() {
+
+}
+
+function my_header_logout() {
+    console.log("kk");
 }
