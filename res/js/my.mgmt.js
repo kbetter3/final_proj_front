d0869a51b74d476093c12a776e662ed8b0e5a551
kbetter3 <kbetter3@naver.com>
2018-06-18 13:17:30 +0900

    albummgmt, albumupload 뷰 연결

--- res/js/my.mgmt.js
+++ res/js/my.mgmt.js
@@ -1,4 +1,4 @@
-// 가수/그룹 관리페이지 로드
+// 가수/그룹 관리페이지 불러오기
 function my_mgmt_artistmgmt() {
     $.ajax({
         url: "artistmgmt",
@@ -37,5 +37,87 @@ function my_mgmt_artistupload() {
 }
 
 function my_mgmt_success_artisupload(jobj) {
+    $("#my-contents-container").html("").append(jobj.tags);
+}
+
+
+// 앨범 관리페이지 불러오기
+function my_mgmt_albummgmt() {
+    $.ajax({
+        url: "mgmt/albummgmt",
+        success: my_mgmt_success_albummgmt
+    });
+}
+
+function my_mgmt_success_albummgmt(jobj) {
+    if (jobj.state == RespState.tags) {
+        $("#my-contents-container").html("").append(jobj.tags);
+        my_mgmt_albumcont();
+    } else {
+        console.log("권한이 없습니다.");
+    }
+}
+// 앨범 컨텐츠 블러오기
+function my_mgmt_albumcont() {
+    $.ajax({
+        url: "mgmt/albumcont",
+        success: my_mgmt_success_albumcont
+    });
+}
+
+function my_mgmt_success_albumcont(jobj) {
+    console.log("로드되었음");
+}
+
+// 앨범 업로드페이지 불러오기
+function my_mgmt_albumupload() {
+    $.ajax({
+        type: "GET",
+        url: "mgmt/albumupload",
+        success: my_mgmt_success_albumupload
+    });
+}
+function my_mgmt_success_albumupload(jobj) {
+    $("#my-contents-container").html("").append(jobj.tags);
+}
+
+
+// 음원 관리페이지 불러오기
+function my_mgmt_musicmgmt() {
+    $.ajax({
+        url: "mgmt/musicmgmt",
+        success: my_mgmt_success_musicmgmt
+    });
+}
+
+function my_mgmt_success_musicmgmt(jobj) {
+    if (jobj.state == RespState.tags) {
+        $("#my-contents-container").html("").append(jobj.tags);
+        my_mgmt_musiccont();
+    }
+}
+
+// 음원 컨텐츠 불러오기
+function my_mgmt_musiccont() {
+    $.ajax({
+        url: "mgmt/musiccont",
+        success: my_music_success_musiccont
+    });
+}
+
+function my_mgmt_success_musiccont(jobj) {
+    console.log("로드되었음")
+}
+
+// 음원 업로드페이지 불러오기
+function my_mgmt_musicupload() {
+    $.ajax({
+        type: "GET",
+        url: "mgmt/musicupload",
+        success: my_mgmt_success_musicupload
+    });
+}
+
+function my_mgmt_success_musicupload(jobj) {
     $("#my-contents-container").html("").append(jobj.tags);
 }
