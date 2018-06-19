e5fadef83cbb1ea3f01607243d663b60e1728e22
kbetter3 <kbetter3@naver.com>
2018-06-19 19:34:05 +0900

    artistupload에 사진 reflect 수정함

--- res/js/my.artistupload.js
+++ res/js/my.artistupload.js
@@ -8,16 +8,16 @@ function my_artistupload_radio() {
 }
 
 function my_artistupload_picture_select() {
+//    var formData = new FormData($("#upload-form")[0]);
     var formData = new FormData();
     formData.append("pic", $(this)[0].files[0]);
 
     $.ajax({
         url: "pictest",
-        data: {
-            mFile : formData
-        },
-        processData: false,
+        data: formData,
+        cache: false,
         contentType: false,
+        processData: false,
         type: "POST",
         success: my_artistupload_success_picture_select
     });
@@ -26,5 +26,56 @@ function my_artistupload_picture_select() {
 function my_artistupload_success_picture_select(jobj) {
     var fname = jobj.data;
 
-    $("#my-artistupload-img").css("background-image", "pictest2?fname="+fname);
+    console.log("my-upload-artist-img에 src 걸기");
+    console.log(jobj.data);
+    $("#my-upload-artist-img").attr("src", "pictest2?fname=" + jobj.data);
+}
+
+function my_artistupload_upload() {
+    var name = $("#my-upload-artist-name");
+    var member = $("#my-upload-artist-member");
+    var debutdate = $("#my-upload-artist-debut");
+    var activitytype = $(".my-upload-input-radio[checked=checked]");
+    var agency = $("#my-upload-artist-company");
+    var thumb = $("#my-upload-artist-picture");
+
+    var bname = name.val().length > 0 ? true : false;
+    var bmember = member.val().length > 0 ? true : false;
+    var bdebutdate = debutdate.val().length > 0 ? true : false;
+    var bagency = agency.val().length > 0 ? true : false;
+    var bthumb = thumb[0].files[0] != undefined ? true : false;
+
+
+
+    if (bname && bmember && bdebutdate && bagency && bthumb) {
+        console.log("등록하자");
+
+//        var formData = new FormData();
+//        formData.append("name", name.val());
+//        formData.append("member", member.val());
+//        formData.append("debutdate", debutdate.val());
+//        formData.append("activitytype", activitytype.val());
+//        formData.append("agency", agency.val());
+//        formData.append("img", thumb.val());
+        var formData = new FormData($("form")[0]);
+
+        $.ajax({
+            url: "mgmt/artistupload",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            data: formData,
+            success: my_artistupload_success_artistupload
+        });
+    } else {
+        console.log(bname);
+        console.log(bmember);
+        console.log(bdebutdate);
+        console.log(bagency);
+        console.log(bthumg);
+    }
+}
+
+function my_artistupload_success_artistupload(jobj) {
+    console.log("artistupload 성공했다 artistmgmt로 보내라");
 }
