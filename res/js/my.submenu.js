2e446876c3517fed03731a7076adccf701be7f9e
kbetter3 <kbetter3@naver.com>
2018-06-20 19:49:38 +0900

    서브메뉴에서 음원데이터 요청 및 응답처리 완료, 받은 데이터를 화면에 뿌려주어야함

--- res/js/my.submenu.js
+++ res/js/my.submenu.js
@@ -2,8 +2,20 @@ function my_submenu_submenu(target) {
     $.ajax({
         url: "submenu",
         data: {fname: target},
+        async: true,
         success: my_submenu_success_submenu
     });
+
+    var chartcontent;
+    if (target == "chartsubmenu") {
+        chartcontent = "chartrealtime";
+    } else if (target == "lastestsubmenu") {
+        chartcontent = "lastestdom";
+    } else if (target == "genresubmenu") {
+        chartcontent = "genreballad";
+    }
+
+    my_chart_chart(chartcontent, 1);
 }
 
 function my_submenu_success_submenu(jobj) {
@@ -12,6 +24,8 @@ function my_submenu_success_submenu(jobj) {
     } else {
         console.log("로그인 전용 메뉴");
     }
+
+
 }
 
 
