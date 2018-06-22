90aaf3a92b1385e157a2f940c7aec0bc74e2f2bc
kbetter3 <kbetter3@naver.com>
2018-06-22 21:39:39 +0900

    myinfo 데이터 로드 및 배치

--- res/js/my.myinfo.js
+++ res/js/my.myinfo.js
@@ -1,3 +1,29 @@
 function my_myinfo_load() {
-    console.log("불러와졌다");
+    my_myinfo_loaddata();
+
+    $("#my-myinfo-uploader").on("click", my_myinfo_uploader);
+}
+
+function my_myinfo_loaddata() {
+    $.ajax({
+        type: "post",
+        url: "member/myinfo",
+        success: my_myinfo_success_loaddata
+    });
+}
+
+function my_myinfo_success_loaddata(jobj) {
+    if (jobj.state == RespState.data) {
+        var member = jobj.member;
+
+        $("#my-myinfo-id").val(member.id);
+        $("#my-myinfo-email").val(member.email);
+        $("#my-myinfo-power").val(member.power);
+        $("#my-myinfo-regdate").val(member.regdate);
+        $("#my-myinfo-voucher").val(member.voucher);
+        $("#my-myinfo-downcount").val(member.downcount);
+    }
+}
+
+function my_myinfo_uploader() {
 }
