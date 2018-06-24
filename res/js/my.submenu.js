be747f893a208331d817b4041e669386a8b643be
wooba93 <wooba93@gmail.com>
2018-06-24 15:46:33 +0900

    메뉴 색상 변경

--- res/js/my.submenu.js
+++ res/js/my.submenu.js
@@ -34,6 +34,11 @@ function my_submenu_changeSubmenu(target, pg) {
     my_chart_chart($(this).attr("controller"), 1);
 }
 
+// 메뉴 색상 변경
+function my_submenu_change_menu_color(e){
+    $(".my-submenu-container li").removeClass("my-submenu-selected")
+    $(e.currentTarget).parent("li").addClass("my-submenu-selected")
+}
 
 // 가수/그룹 서브메뉴 클릭시
 function my_submenu_artistmgmt() {
